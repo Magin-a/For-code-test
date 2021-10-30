193bac335252de9b6a46013ef7bf31dda62326f6
Magin-a <80820166+Magin-a@users.noreply.github.com>
2021-10-30 13:34:23 +0900

    3-3

--- Part2(주요알고리즘 )/그리디/숫자 카드 게임.py
+++ Part2(주요알고리즘 )/그리디/숫자 카드 게임.py
@@ -1,1 +1,12 @@
 #예제3-3(숫자 카드 게임)
+m, n  = map(int, input().split())
+board = []
+result = 0
+for _ in range(m):
+    num = list(map(int, input().split()))
+    pre = result
+    result = min(num)
+    if pre >= result:
+        result = pre
+
+print(result)
